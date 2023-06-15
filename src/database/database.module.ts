5bbe292866d95c8c4e28206a160b0d173f10a108
Carlos Aguilar <caguilar@redpacifico.com>
2023-06-15 09:06:47 -0700

    fix: remove unnecesay code

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -5,14 +5,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import config from '../config';
 
-const API_KEY = '12345634';
-const API_KEY_PROD = 'PROD1212121SA';
-
-// client.query('SELECT * FROM tasks', (err, res) => {
-//   console.error(err);
-//   console.log(res.rows);
-// });
-
 @Global()
 @Module({
   imports: [
@@ -35,10 +27,6 @@ const API_KEY_PROD = 'PROD1212121SA';
   ],
   providers: [
     {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
-    {
       provide: 'PG',
       useFactory: (configService: ConfigType<typeof config>) => {
         const { user, host, dbName, password, port } = configService.postgres;
@@ -55,6 +43,6 @@ const API_KEY_PROD = 'PROD1212121SA';
       inject: [config.KEY],
     },
   ],
-  exports: ['API_KEY', 'PG', TypeOrmModule],
+  exports: ['PG', TypeOrmModule],
 })
 export class DatabaseModule {}
